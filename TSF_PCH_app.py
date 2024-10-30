71924ea9cf335c4867781f09960bd8a0db5c01e0
VatIADev <vatiasolutions@gmail.com>
2024-10-30 00:03:28 -0500

    Update TSF_PCH_app.py

--- TSF_PCH_app.py
+++ TSF_PCH_app.py
@@ -9,6 +9,9 @@ set_random_seed(42)
 
 @st.cache_data
 def carga_archivos(archivo):
+    required_columns = ["TIPO", "PLANTA", "VERSION", "FECHA", "H01", "H02", "H03", "H04", "H05",
+                        "H06", "H07", "H08", "H09", "H10", "H11", "H12", "H13", "H14", "H15", 
+                        "H16","H17", "H18", "H19", "H20", "H21", "H22", "H23", "H24"]
     DB = ""
     if archivo is not None:
         df = pd.read_csv(archivo)
